975170d1c02a8f22c2f775326a280aa697c102f8
Cedric Ewen <cedric.ewen@desy.de>
2023-07-25 16:47:43 +0200

    add fourvectors to output

--- prepare_data_fastjet.py
+++ prepare_data_fastjet.py
@@ -60,8 +60,8 @@ def main():
     # Actually cluster the data
     # this is what takes a long time
     print("Clustering...")
-    out_qcd = cluster(qcd_data, n_events=len(qcd_data))
-    out_sig = cluster(sig_data, n_events=len(sig_data))
+    out_qcd = cluster(qcd_data, n_events=1000)  # len(qcd_data))
+    out_sig = cluster(sig_data, n_events=1000)  # len(sig_data))
 
     print("Processing...")
     for c, jets in enumerate([out_qcd, out_sig]):
@@ -87,12 +87,17 @@ def main():
             # get padded constituents, relative coordinates and mask in the wanted format
             len_padding = 300
             constituents = []
+            constituents_xyze = []
             rel_constituents = []
             mask = []
             jet_mass = []
             jet_pt = []
             jet_eta = []
             jet_phi = []
+            jet_px = []
+            jet_py = []
+            jet_pz = []
+            jet_e = []
             for jet in xy_jets:
                 # get constituents
                 const_pt = np.array(
@@ -107,8 +112,27 @@ def main():
                 const_phi = np.array(
                     [(jet.constituents()[i].phi_std()) for i in range(len(jet.constituents()))]
                 )
+                # xyze
+                const_px = np.array(
+                    [(jet.constituents()[i].px()) for i in range(len(jet.constituents()))]
+                )
+                const_py = np.array(
+                    [(jet.constituents()[i].py()) for i in range(len(jet.constituents()))]
+                )
+                const_pz = np.array(
+                    [(jet.constituents()[i].pz()) for i in range(len(jet.constituents()))]
+                )
+                const_e = np.array(
+                    [(jet.constituents()[i].e()) for i in range(len(jet.constituents()))]
+                )
+
+                # concatenate
                 consts = np.concatenate(
                     (const_pt[:, None], const_eta[:, None], const_phi[:, None]), axis=1
+                )
+                consts_xyze = np.concatenate(
+                    (const_px[:, None], const_py[:, None], const_pz[:, None], const_e[:, None]),
+                    axis=1,
                 )
                 # sort constituents by pT from high to low
                 consts = consts[np.argsort(consts[:, 0])[::-1]]
@@ -116,6 +140,12 @@ def main():
                 # pad constituents and mask
                 padded_consts = np.pad(
                     consts, ((0, len_padding - len(consts)), (0, 0)), "constant", constant_values=0
+                )
+                padded_consts_xyze = np.pad(
+                    consts_xyze,
+                    ((0, len_padding - len(consts_xyze)), (0, 0)),
+                    "constant",
+                    constant_values=0,
                 )
                 padded_mask = np.pad(
                     np.ones(len(consts)),
@@ -147,18 +177,38 @@ def main():
                 jet_pt.append(jet.perp())
                 jet_eta.append(jet.pseudorapidity())
                 jet_phi.append(jet.phi_std())
+                jet_px.append(jet.px())
+                jet_py.append(jet.py())
+                jet_pz.append(jet.pz())
+                jet_e.append(jet.e())
                 constituents.append(padded_consts)
+                constituents_xyze.append(padded_consts_xyze)
                 rel_constituents.append(rel_constituents_temp)
                 mask.append(padded_mask)
             mask = np.array(mask)
             constituents = np.array(constituents) * mask[:, :, None]
+            constituents_xyze = np.array(constituents_xyze)
             rel_constituents = np.array(rel_constituents) * mask[:, :, None]
             jet_mass = np.array(jet_mass)
             jet_pt = np.array(jet_pt)
             jet_eta = np.array(jet_eta)
             jet_phi = np.array(jet_phi)
+            jet_px = np.array(jet_px)
+            jet_py = np.array(jet_py)
+            jet_pz = np.array(jet_pz)
+            jet_e = np.array(jet_e)
             jet_data = np.concatenate(
-                (jet_pt[:, None], jet_eta[:, None], jet_phi[:, None], jet_mass[:, None]), axis=1
+                (
+                    jet_pt[:, None],
+                    jet_eta[:, None],
+                    jet_phi[:, None],
+                    jet_mass[:, None],
+                    jet_px[:, None],
+                    jet_py[:, None],
+                    jet_pz[:, None],
+                    jet_e[:, None],
+                ),
+                axis=1,
             )
 
             # remove constituents with 0 pT and very high etas
@@ -173,6 +223,7 @@ def main():
             print(f"removed {counter} constituents")
 
             print(f"constituents shape: {constituents.shape}")
+            print(f"constituents_xyze shape: {constituents_xyze.shape}")
             print(f"rel_constituents shape: {rel_constituents.shape}")
             print(f"jet_data shape: {jet_data.shape}")
             print(f"mask shape: {mask.shape}")
@@ -181,11 +232,13 @@ def main():
 
             if c == 0:
                 constituents_x = constituents.copy()
+                constituents_xyze_x = constituents_xyze.copy()
                 rel_constituents_x = rel_constituents.copy()
                 jet_data_x = jet_data.copy()
                 mask_x = mask.copy()
             else:
                 constituents_y = constituents.copy()
+                constituents_xyze_y = constituents_xyze.copy()
                 rel_constituents_y = rel_constituents.copy()
                 jet_data_y = jet_data.copy()
                 mask_y = mask.copy()
@@ -193,10 +246,12 @@ def main():
         final_path = filepath_save + f"_{signal_type}.h5"
         with h5py.File(final_path, "w") as f:
             f.create_dataset("constituents_x", data=constituents_x)
+            f.create_dataset("constituents_xyze_x", data=constituents_xyze_x)
             f.create_dataset("rel_constituents_x", data=rel_constituents_x)
             f.create_dataset("jet_data_x", data=jet_data_x)
             f.create_dataset("mask_x", data=mask_x)
             f.create_dataset("constituents_y", data=constituents_y)
+            f.create_dataset("constituents_xyze_y", data=constituents_xyze_y)
             f.create_dataset("rel_constituents_y", data=rel_constituents_y)
             f.create_dataset("jet_data_y", data=jet_data_y)
             f.create_dataset("mask_y", data=mask_y)
